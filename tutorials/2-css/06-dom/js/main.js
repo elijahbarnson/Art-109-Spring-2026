77eca7a31e4a3a794abcdd7e62fc744a30f9c907
elijahbarnson <elijah.barnson@sjsu.edu>
2026-02-27 11:45:58 -0800

    tut 9 dom events

---
+++ tutorials/2-css/06-dom/js/main.js
@@ -0,0 +1,43 @@
+// console.log("hello")
+
+// select html elements
+const header = document.querySelector('#header');
+const changeHeaderButton = document.querySelector('#change-header-button');
+const changeThemeButton = document.querySelector('#change-theme-button');
+const img1 = document.querySelector("#img1");
+const img2 = document.querySelector("#img2");
+const img3 = document.querySelector("#img3");
+
+// change header with button click
+changeHeaderButton.addEventListener("click", () => {
+    header.innerHTML = "POW!!!";
+})
+
+// toggle color theme
+
+// create function for changing button text
+function changeButtonText(){
+    if (document.body.classList.contains('dark')) {
+        changeThemeButton.textContent = "Switch to Light Theme"
+    } else {
+            changeThemeButton.textContent = "Switch to Dark Theme"
+    }
+    
+}
+
+// click even on button
+changeThemeButton.addEventListener('click', () => {
+    // add or remove dark class to body
+    document.body.classList.toggle('dark')
+    changeButtonText();
+})
+
+// toggle image visibility
+
+img1.addEventListener("click", () => {
+    img2.classList.remove("hidden");
+})
+
+img2.addEventListener("click", () => {
+    img3.classList.remove("hidden");
+})
